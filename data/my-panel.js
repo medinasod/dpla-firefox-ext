5346733ddb4461f53dab3402de0f00cd40461b25
Rob <rjbain@usfca.edu>
2016-03-15 10:49:34 -0700

    fixing image urls

--- data/my-panel.js
+++ data/my-panel.js
@@ -9,18 +9,17 @@ self.port.on("sendJSON", function(json) {
     json.docs.forEach(function(items) {
         title = JSON.stringify(items.sourceResource.title);
         desc = JSON.stringify(items.sourceResource.description);
-        img = JSON.stringify(items.object);       
-        console.log(img);
+        image = items.object;       
+        console.log(image);
         var node = document.createElement("li");                 // Create a <li> node
         var imgTag = document.createElement("img");                 // Create a <img> node
-        imgTag.setAttribute('src', img);
+        imgTag.setAttribute('src', image);
         imgTag.setAttribute('alt', desc);
-        imgTag.style.width= '25px';
         titleTextNode = document.createTextNode(title);
         descTextNode = document.createTextNode(desc);
         node.appendChild(titleTextNode);                         // Append the text to <li>
         node.appendChild(descTextNode);                          // Append the text to <li>
-        document.getElementById("myList").appendChild(node);     // Append <li> to <ul> with id="myList"
-        document.getElementById("myImgs").appendChild(imgTag);
+        //document.getElementById("myList").appendChild(node);     // Append <li> to <ul> with id="myList"
+        document.getElementById("photos").appendChild(imgTag);
     });
 });
